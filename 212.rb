c033d7ac2b98608a2d243cb7845dae91946d9515
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-18 16:02:54 -0400

    Add them again

---
+++ 212.rb
@@ -0,0 +1,16 @@
+# QUESTION 1
+# Write code to store a number using a variable, then a string using a different variable.
+# Then add the two variables together (by converting the number to a string) and 
+# store the result in a third variable. Finally, print out the third variable.
+number = 12 
+string = "How many months are there in a year? "
+sentence = string + number.to_s 
+puts sentence 
+# QUESTION 2
+# The code below has a mistake in it. Run the code to see the error message,
+# then explain what the error message means and what would need to change to fix it.
+x = 10
+puts x + y
+y = x
+
+
