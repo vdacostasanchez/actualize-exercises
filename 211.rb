b889c43af1a7754d04c21277fbd9f53b43237a3f
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-18 12:47:37 -0400

    Add 212.rb

---
+++ 211.rb
@@ -0,0 +1,22 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+while true #makes the loop run furever
+    puts "Enter a word: "
+    word = gets.chomp
+    if word == "stop"
+        break 
+    end 
+end 
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+#I was able to solve the problem from memory but I had trouble remembering
+#if after the if statement there was supposed to be an else statement or not
+#I did not put it and it turned out to be right. 
+#I will be practicing the next point in the loops category
+#Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters
+
