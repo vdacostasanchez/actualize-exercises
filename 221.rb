c3dd7099aae2dfbff820fe19134008e1843305b9
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-19 19:58:00 -0400

    Add deliberate practice problems

---
+++ 221.rb
@@ -0,0 +1,18 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+array_letters = ["a", "b", "c", "d"]
+array_letters[1] = "e"
+index = 0
+while index < 4 
+    print array_letters[index]
+    index = index + 1
+end 
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
