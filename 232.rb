5fb9697c759ea91876be475e67541cb7d2fe85c2
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-20 09:43:20 -0400

    Add 232.rb

---
+++ 232.rb
@@ -0,0 +1,22 @@
+# QUESTION 1
+# Write code that prints all the years between 1980 to 2020 (including 1980 and 2020).
+year = 1980 
+while year <= 2020
+    puts year
+    year = year + 1
+end 
+
+# QUESTION 2
+# The following code is supposed to print the numbers 1 through 10, but it has errors.
+# Fix the errors with the code below.
+count = 0
+while count > 10 #To print the numbers through 10 the condition should be less than or equal
+  puts count 
+  #You have to add to count each time to keep iterating through the loop
+end
+#Correct code 
+count = 0
+while count <= 10
+    puts count
+    count = count + 1
+end 
