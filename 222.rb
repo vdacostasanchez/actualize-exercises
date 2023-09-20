a611d8373ef55d40d6e5a9efd41ff2ca91affc75
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-19 20:07:42 -0400

    Add problem 222.rb

---
+++ 222.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# Write a program that asks the user for their name.
+# If their name is Martin, print "Hi Martin!" to the screen.
+# Otherwise print "You're not Martin."
+puts "Enter your name: "
+name = gets.chomp
+if name == "Martin"
+    puts "Hi Martin!"
+else 
+    puts "You're not Martin"
+end
+
+# QUESTION 2
+# Fix the indentation and the errors with the code below.
+#x = 1000
+#if x < 5
+#puts 0
+#else if x < 10
+#puts 1
+#else x < 100
+#puts 2
