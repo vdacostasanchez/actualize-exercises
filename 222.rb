57a1610e431858fb581761e8e21a991e3746f302
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-19 20:10:50 -0400

    Add problem 222.rb

--- 222.rb
+++ 222.rb
@@ -14,8 +14,18 @@ end
 # Fix the indentation and the errors with the code below.
 #x = 1000
 #if x < 5
-#puts 0
-#else if x < 10
-#puts 1
-#else x < 100
-#puts 2
+#puts 0    needs to be indented 
+#else if x < 10   else if does not work, it needs to be changed to elsif
+#puts 1    needs to be indented
+#else x < 100     a condition is not needed after an else statement 
+#puts 2    needs to be indented
+#there needs to be an end to the if statements 
+#Correct code
+x = 1000
+if x < 5
+    puts 0
+elsif x < 10
+    puts 1
+else 
+    puts 2
+end 
