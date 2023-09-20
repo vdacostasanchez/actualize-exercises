68bf0cd28396e62180b4944f4b97b4a5e6e07fed
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-20 09:34:40 -0400

    Add practice problems

---
+++ 231.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+#Create an array to store 1 number. Then add three more numbers to the array and print the array on one line
+array = [1]
+number = 1
+while number < 4
+    array[number] = number 
+    number = number + 1
+end 
+index = 0 
+while index < 5
+    print array[index]
+    index = index + 1
+end 
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
