e7cc6d47b4b3d1c669cbc9323f6d8e65fa9dd032
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-20 09:37:03 -0400

    Add practice problems

--- 231.rb
+++ 231.rb
@@ -2,17 +2,12 @@
 # Paste a deliberate practice question you are currently working on below.
 # Write the code to solve the problem without looking at any notes or other resources.
 #Create an array to store 1 number. Then add three more numbers to the array and print the array on one line
-array = [0]
-number = 1
-while number <= 4
-    array[number] = number 
-    number = number + 1
-end 
-index = 0 
-while index < 5
-    print array[index]
-    index = index + 1
-end 
+numbers = [0]
+numbers.push(1)
+numbers.push(2)
+numbers.push(3)
+numbers.push(4)
+pp numbers
 
 # Were you able to easily solve the problem from memory?
 # If so, describe the next skill you will start to practice tomorrow.
