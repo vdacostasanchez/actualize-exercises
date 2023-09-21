7a7d5269b14e2596d079b82d42ba731200ad3e0f
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-21 13:10:37 -0400

    Add 241.rb

--- 241.rb
+++ 241.rb
@@ -1,7 +1,10 @@
 # DELIBERATE PRACTICE REFLECTION
 # Paste a deliberate practice question you are currently working on below.
 # Write the code to solve the problem without looking at any notes or other resources.
-
+#Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+countries = ["Mexico", "Colombia", "Uruguay"]
+countries.push("USA")
+pp countries
 
 # Were you able to easily solve the problem from memory?
 # If so, describe the next skill you will start to practice tomorrow.
