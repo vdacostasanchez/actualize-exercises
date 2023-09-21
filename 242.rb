912697f3538003e40faf14210ca1b19fe1149a93
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-21 09:45:07 -0400

    Add week 2 day 4 problems

---
+++ 242.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+
