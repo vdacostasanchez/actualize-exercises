510233ddc41c306d7545feb045e11d83cd35d764
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-21 13:19:39 -0400

    Add 242.rb

--- 242.rb
+++ 242.rb
@@ -8,5 +8,7 @@ pp words
 # QUESTION 2
 # Use a variable to store a hash with two different words and their definitions.
 # Then add one more word with its definition and print the hash on one line.
-
+words = {"happy" => "feeling or showing pleasure or contentment", "sad" => "feeling or showing sorrow"}
+words["angry"] = "feeling or showing strong annoyance, displeasure, or hostility"
+pp words
 
