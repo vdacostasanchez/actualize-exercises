bc33438599f7ec8241e2ab2986b343ace2b634b7
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-21 13:13:51 -0400

    Add 242.rb

--- 242.rb
+++ 242.rb
@@ -1,7 +1,9 @@
 # QUESTION 1
 # Use a variable to store an array with three different words.
 # Then add one more word to the array and print the array on one line.
-
+words = ["Hello", "Perfect", "Love"]
+words.push("Crazy")
+pp words
 
 # QUESTION 2
 # Use a variable to store a hash with two different words and their definitions.
