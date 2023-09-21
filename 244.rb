38441393b265270566b3b59dcc440cf187344011
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-21 13:31:39 -0400

    Add 244.rb

--- 244.rb
+++ 244.rb
@@ -6,7 +6,9 @@
 drink1 = {"name" => "Seltzer", "ingredients" => ["water", "bubbles"]}
 drink2 = {"name" => "Coca Cola", "ingredients" => ["water", "bubbles", "sugar", "brown food coloring"]}
 drink3 = {"name" => "water", "ingredients" => ["hydrogen", "oxygen"]}
-
+puts drink1["name"]
+pp drink1["ingredients"]
+puts drink1["ingredients"][0]
 
 # QUESTION 2
 # Start with the array of hashes below.
