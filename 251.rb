f1feab5155baa62c984d548c059fe696b69418a9
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-22 14:26:53 -0400

    Add 251.rb

--- 251.rb
+++ 251.rb
@@ -1,10 +1,19 @@
 # DELIBERATE PRACTICE REFLECTION
 # Paste a deliberate practice question you are currently working on below.
 # Write the code to solve the problem without looking at any notes or other resources.
-
+# Start with an array of numbers and create a new array with each number times 3.
+# For example, [1, 2, 3] becomes [3, 6, 9]
+numbers = [ 1, 2, 3]
+index = 0
+while index < 3
+    numbers[index] = numbers[index] * 3
+    index = index + 1
+end 
+pp numbers
 
 # Were you able to easily solve the problem from memory?
 # If so, describe the next skill you will start to practice tomorrow.
 # If not, describe the part you got stuck on that requires more practice.
+#I did do it from memory. Replacing the same index was a bit confusing. 
 
 
