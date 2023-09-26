9071e7a8c7489b962db63794389a558797ad5829
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-25 21:55:14 -0400

    Add 311.rb

---
+++ 311.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+hashes = [
+    {name: "Alice", age: 27},
+    {name: "Blane", age: 16},
+]
+names = []
+index = 0
+while index < hashes.length
+    names << hashes[index][:name]
+    index = index + 1
+end 
+pp names
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+#I was able to do it from memory. I want to continue practicing arrays and hashes used together. 
+
