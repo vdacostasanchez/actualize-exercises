81bbc66adccee511fca4ac91e561c463782394f8
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-27 09:49:49 -0400

    Add 332.rb

--- 332.rb
+++ 332.rb
@@ -1,5 +1,10 @@
 # QUESTION 1
 # Use a loop to compute the sum of all the given numbers.
 numbers = [5, 4, 2, 2, 6, 8, 1]
-
-
+sum = 0
+index = 0
+while index < numbers.length
+    sum = sum + numbers[index]
+    index += 1
+end 
+puts sum
