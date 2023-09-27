28d22254f77cfdb2ab8b70b20c86a72e9b2e4bc4
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-27 09:42:34 -0400

    Add week 3 day 3 exercises

---
+++ 332.rb
@@ -0,0 +1,5 @@
+# QUESTION 1
+# Use a loop to compute the sum of all the given numbers.
+numbers = [5, 4, 2, 2, 6, 8, 1]
+
+
---
+++ 333.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Create a program that asks the user to enter 5 numbers. 
+# Then, tell the user the mean average of all the numbers.
+# The mean average is defined as the sum of all the numbers 
+# divided by the count of how many numbers there are. 
+# For example, if the user enters: 11, 7, 30, 22, 55
+# the average will be 25. This is because 11 + 7 + 30 + 22 + 55 = 125, and 125 / 5 
+# (the amount of numbers in the list) = 25.
+
+# Note: If you run the code p 9 / 2, it prints 4 even though the result should be 4.5. 
+# This is because when dividing integers in Ruby, the result will be an integer. 
+
