c8a9a56794aeb1834157fc50239811b8d4da52d9
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-27 10:32:36 -0400

    Add 334.rb

--- 334.rb
+++ 334.rb
@@ -11,4 +11,57 @@
 # The program will say: "apple was your most common word"
 # (That's because apple appeared in the user's list 4 times, more than any other word.)
 
+#Have the user enter a word
+# puts "Enter a word: "
+
+#Add word to an array
+# puts "Enter a word: "
+# word = gets.chomp 
+# words = []
+# words << word 
+# pp words
+
+#Create a while loop that asks the user 10 times for a word and create array for 10 words
+# index = 0
+# words = []
+# while index < 10 
+#     puts "Enter a word: "
+#     word = gets.chomp 
+#     words << word 
+#     index += 1
+# end 
+# pp words
+
+#Create a loop with conditional inside to compare values 
+index = 0
+words = []
+while index < 10 
+    puts "Enter a word: "
+    word = gets.chomp 
+    words << word 
+    index += 1
+end 
+pp words
+index = 0
+most_common = words[0]
+repetitions = 0
+most_common_repetitions = 0
+while index < words.length
+    index2 = 0
+    repetitions = 0
+    while index2 < words.length
+        if words[index] == words[index2]
+            repetitions += 1
+        end 
+        index2 += 1
+    end 
+    puts repetitions
+    if repetitions > most_common_repetitions
+        most_common_repetitions = repetitions
+        most_common = words[index]
+    end 
+    index += 1
+end 
+puts "#{most_common} was your most common word"
+
 
