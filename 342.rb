93d56ab28a718a40f57dc0b5eb85f9369d949551
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-28 09:45:41 -0400

    Add 342.rb

--- 342.rb
+++ 342.rb
@@ -1,5 +1,14 @@
 # QUESTION 1
 # Use a loop to create a new array with only the strings with 6 or fewer letters.
 words = ["correct", "horse", "battery", "staple"]
-
+index = 0
+short_words = []
+while index < words.length
+    word = words[index]
+    if word.length <= 6 
+        short_words << word 
+    end 
+    index += 1
+end 
+pp short_words
 
