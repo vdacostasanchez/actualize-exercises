92bc62d9cd6ea0428a9d3555dbf3d245580aba89
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-29 09:19:33 -0400

    Add week 2 day 5 exercises

---
+++ 352.rb
@@ -0,0 +1,5 @@
+# QUESTION 1
+# Use a loop to create a new array with each string's first letter only.
+words = ["Big", "Elephants", "Can", "Always", "Understand", "Small", "Elephants"]
+
+
---
+++ 354.rb
@@ -0,0 +1,21 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Ruby has a .sort method that makes it easy to sort an array. 
+# The goal of this exercise is to implement your own sort from scratch 
+# (without using Ruby's #sort method!)
+
+# The approach you will implement is called bubble sort. 
+# First you examine the first two items in the array. 
+# If the second is smaller than the first, you swap their positions. 
+# Then you move on to the next two items and keep repeating. 
+# You can see a visual of it in action here: https://upload.wikimedia.org/wikipedia/commons/c/c8/Bubble-sort-example-300px.gif 
+
+# Your job is to implement a bubble sort that takes an array and returns a sorted array. 
+# To be clear, you're not allowed to use the built in .sort method.
+# Instead you'll write code to go through the array two items at a time 
+# to accomplish the same thing.
+
+
