d7a82af7fcc6fa32d4780e8a10bd6e6d5d3c2ffe
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-29 09:27:10 -0400

    Add 352.rb

--- 352.rb
+++ 352.rb
@@ -1,5 +1,12 @@
 # QUESTION 1
 # Use a loop to create a new array with each string's first letter only.
 words = ["Big", "Elephants", "Can", "Always", "Understand", "Small", "Elephants"]
+first_letters = []
+index = 0
+while index < words.length
+    first_letter = words[index][0]
+    first_letters << first_letter
+    index += 1
+end 
 
-
+pp first_letters
