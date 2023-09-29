00103b841d15c6926c5d281b7248ad4d0e1d50e0
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-09-29 09:29:19 -0400

    Add 353.rb

--- 353.rb
+++ 353.rb
@@ -3,5 +3,11 @@
 # The rest of your program should print out the second letter from each inner array. 
 # (The result in your terminal should be the letters b, d, f, h, j on separate lines)
 letters = [["a", "b"], ["c", "d"], ["e", "f"], ["g", "h"], ["i", "j"]]
-
-
+second_letters = []
+index = 0
+while index < letters.length
+    second_letter = letters[index][1]
+    second_letters << second_letter
+    index += 1
+end 
+pp second_letters
