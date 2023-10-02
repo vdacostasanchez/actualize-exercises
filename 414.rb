32bec7123a79027b38885d367905f1da408b3cfe
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-02 09:51:57 -0400

    Add 414.rb

--- 414.rb
+++ 414.rb
@@ -3,4 +3,10 @@
 # Then write a program that creates a file called example.txt with the 
 # sentence "This is an example!" in it.
 
-
+# Open the file in write mode (creates the file if it doesn't exist)
+File.open("example.txt", "w+") do |file|
+    # Write the sentence to the file
+    file.write("This is an example!")
+end
+  
+  
