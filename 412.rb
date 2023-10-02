d8d63d8e87b286641a05c34104d8fc3dcefdc6af
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-02 09:22:24 -0400

    Add 412.rb

--- 412.rb
+++ 412.rb
@@ -1,5 +1,14 @@
 # QUESTION 1
 # Write a method that takes in an array of numbers and returns its sum.
 # Then run the method and print the result.
+def sum(numbers)
+    index = 0
+    add = 0
+    while index < numbers.length
+        add += numbers[index]
+        index += 1
+    end 
+    return add 
+end 
 
-
+puts sum([3,4,5,7])
