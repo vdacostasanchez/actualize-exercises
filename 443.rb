6a62a06ea02e6adce211159834a288353292e6b6
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-05 16:40:38 -0400

    Add 443.rb

--- 443.rb
+++ 443.rb
@@ -2,14 +2,14 @@
 # The following code uses the money Ruby library.
 # First read the library documentation: https://github.com/RubyMoney/money 
 # Then write comments next to each line explaining what you think the code is doing.
-require "money"                          # 
+require "money"                          # need to require the library to use it
 
-I18n.enforce_available_locales = false   # 
-I18n.locale = :en                        # 
-Money.default_currency = "USD"           # 
-Money.add_rate("CAD", "USD", 0.8)        # 
+I18n.enforce_available_locales = false   # allows you to work with any locale even if it is not there 
+I18n.locale = :en                        # sets the locale to English
+Money.default_currency = "USD"           # set the default currency to be USD 
+Money.add_rate("CAD", "USD", 0.8)        # Sets the rate, 1 CAD is equal to 0.8 USD
 
-money1 = Money.new(1000)                 # 
-money2 = Money.new(500, "CAD")           # 
-result = money1 + money2                 # 
-puts "Total: #{result}"                  # 
+money1 = Money.new(1000)                 # new Money variable set to 1000 USD
+money2 = Money.new(500, "CAD")           # new Money variable set to 500 CAD
+result = money1 + money2                 # adds the two monetary values using the rate
+puts "Total: #{result}"                  # prints the total amount
