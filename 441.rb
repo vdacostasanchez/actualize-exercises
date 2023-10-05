93650477e06c877d380870d8d356ddd3f45fb0ad
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-05 16:25:41 -0400

    Add 441.rb

--- 441.rb
+++ 441.rb
@@ -1,10 +1,15 @@
 # DELIBERATE PRACTICE REFLECTION
 # Paste a deliberate practice question you are currently working on below.
 # Write the code to solve the problem without looking at any notes or other resources.
-
+#Write a method that takes in a string and returns the string with all capital letters. Then run the method and print the result.
+def uppercase(string)
+    string = string.upcase
+    return string 
+end 
+puts uppercase("hello")
 
 # Were you able to easily solve the problem from memory?
 # If so, describe the next skill you will start to practice tomorrow.
 # If not, describe the part you got stuck on that requires more practice.
-
+#I was able to do it from memory. I need to practice more complex methods. 
 
