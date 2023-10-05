be7aa6e353d232e8174a89492626da2218db6a77
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-05 16:28:29 -0400

    Add 442.rb

--- 442.rb
+++ 442.rb
@@ -1,5 +1,14 @@
 # QUESTION 1
 # Write a method that takes in an array of numbers and returns an array with every number doubled.
 # Then run the method and print the result.
+def double(numbers)
+    index = 0
+    while index < numbers.length
+        numbers[index] = numbers[index] * 2
+        index += 1
+    end 
+    return numbers
+end
 
+p double([2, 3, 5, 6])
 
