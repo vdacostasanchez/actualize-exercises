b59a87fbfad7ebd1dd16a05a978ccbf80f17c04a
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-05 16:47:44 -0400

    Add 444.rb

--- 444.rb
+++ 444.rb
@@ -2,5 +2,6 @@
 # Read about the Ruby select method: https://www.rubyguides.com/2019/04/ruby-select-method/ 
 # Then write a program that uses the select method with an array of strings 
 # to create a new array with only the strings that start with the letter "a".
-
+words = ["away", "here", "there", "apart", "together"]
+p words.select { |word| word.start_with?("a") }
 
