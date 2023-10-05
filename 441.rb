20eb90186acea561bbe935176f567e1969e463a4
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-05 16:23:21 -0400

    Add 441.rb

---
+++ 441.rb
@@ -0,0 +1,10 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
