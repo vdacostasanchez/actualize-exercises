e5b9348fffa9a2234bf123839347cf06dbf547b0
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-05 16:41:45 -0400

    Add 444.rb

---
+++ 444.rb
@@ -0,0 +1,6 @@
+# QUESTION 1
+# Read about the Ruby select method: https://www.rubyguides.com/2019/04/ruby-select-method/ 
+# Then write a program that uses the select method with an array of strings 
+# to create a new array with only the strings that start with the letter "a".
+
+
