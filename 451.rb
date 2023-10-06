2938ab12190c61928d6b9beb2c07c1a663f9ab1d
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-06 14:54:54 -0400

    Add 451.rb

---
+++ 451.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a method that takes in a string and returns the first letter of the string. Then run the method and print the result.
+def first_letter(word)
+    return word[0]
+end 
+puts first_letter("Hello")
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
