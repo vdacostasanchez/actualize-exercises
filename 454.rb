b48865f15d0eec5bca6e52a6ec177c35d1483f74
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-06 15:23:48 -0400

    Add 454.rb

---
+++ 454.rb
@@ -0,0 +1,8 @@
+# QUESTION 1
+# Read about the Ruby begin and rescue keywords to handle exceptions:
+# https://docs.ruby-lang.org/en/3.2/syntax/exceptions_rdoc.html
+# Then write a program that would crash because of a division by zero error.
+# Instead of crashing with a normal error message, the program should instead
+# print "You cannot divide by zero" to the terminal.
+
+
