756060c5691ac9f28bcdd8b67648cf3d78e72578
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-09 18:25:41 -0400

    Add 511.rb

--- 511.rb
+++ 511.rb
@@ -1,10 +1,18 @@
 # DELIBERATE PRACTICE REFLECTION
 # Paste a deliberate practice question you are currently working on below.
 # Write the code to solve the problem without looking at any notes or other resources.
+# Write a method that takes in a number and returns the number times 10 plus 30. Then run the method and print the result.
+def operation(number)
+    number *= 10 
+    number += 30
+    return number 
+end 
 
+p operation(4)
 
 # Were you able to easily solve the problem from memory?
 # If so, describe the next skill you will start to practice tomorrow.
 # If not, describe the part you got stuck on that requires more practice.
+#I was able to do it from memory. Will now practice integrating all my knowledge, combining methods, arrays, hashes, loops
 
 
