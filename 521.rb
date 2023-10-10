a3f5713e5220435c74915ca153c6d7462ac127db
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-10 18:12:16 -0400

    Add 521.rb

---
+++ 521.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a Rectangle class with attributes for width and height.
+class Rectangle
+    def initialize(input_width, input_height)
+        @width = input_width
+        @height = input_height
+    end
+end 
+
+rectangle = Rectangle.new(13, 2)
+p rectangle
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+#I was not, I forgot that there had to be an initialize method for the class, not just putting next to Rectangle
+
+
+
